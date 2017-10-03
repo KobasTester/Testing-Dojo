1860cadfea9b73db3b968d364b54186548a64255
KobasTester <k.j.charzynski@gmail.com>
2017-10-03 16:10:33 +0200

    pierwszy test

---
+++ AutomationPractice/AutomationPractice/MainPage.cs
@@ -0,0 +1,25 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutomationPractice
+{
+    public class MainPage
+    {
+        static IWebDriver driver = new ChromeDriver();
+
+        public static void GoTo(string url)
+        {
+            driver.Url = url;
+        }
+
+        public static bool Loaded()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ AutomationPractice/Tests/UnitTest1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using AutomationPractice;
+
+namespace Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void Can_Go_To_Main_Page()
+        {
+            MainPage.GoTo("http://automationpractice.com/");
+            Assert.IsTrue(MainPage.Loaded());
+        }
+    }
+}
